69a15d64fcf6bfd202a4f259e921f2dfe50265b6
Bernardus Ari Kuncoro <ari.kuncoro1987@gmail.com>
2016-11-24 12:10:01 +0700

    Change the type of making cluster for UNIX

--- parallel_code_bootstrapping_final.R
+++ parallel_code_bootstrapping_final.R
@@ -2,13 +2,37 @@
 # Source Coude: https://cran.r-project.org/web/packages/doParallel/vignettes/gettingstartedParallel.pdf
 # Comparing the parallel and non parallel programming elapsed time
 # Tested in Windows 10 Desktop OS, R verison 3.2.3 
+# It is also tested in Mac OSX Yosemite version
 
-# 1 Using parallel programming to run glm training of iris dataset 
+
+# First, we need to make sure that all required packages are installed 
+# install packages: parallel, doParallel
+install.packages("parallel")
+install.packages("doParallel")
+
 library(doParallel)
-cl <- makeCluster(3, type = "PSOCK")
+library(parallel)
+
+# Detect 
+no_cores <- detectCores() - 1
+# For Windows, use this:
+# cl <- makeCluster(3, type = "PSOCK")
+
+# For UNIX (Ubuntu, MAC), use this command
+cl <- makeCluster(3, type = "FORK")
+
+# Register the parallel backend with the foreach package.
 registerDoParallel(cl)
 
+# Loading your data (Please check compatibility how to read data from Amazon EC2)
+# iris <- read.csv("iris.csv")
+
+# Assume iris has been loaded 
+
+# 1 Using parallel programming to run glm training of iris dataset 
 x <- iris[which(iris[,5] != "setosa"), c(1,5)]
+
+# Set trial, we can play with this! If we want to have longer test, make it 1000000
 trials <- 10000
 ptime <- system.time({
   r <- foreach(icount(trials), .combine=cbind) %dopar% {
@@ -17,22 +41,25 @@ ptime <- system.time({
   coefficients(result1)
   }
   })[3]
+
+# Function to stop cluster
 stopCluster(cl)
 ptime
 
-# elapsed 
+# In Windows, the elapsed time  
 # 23.25
+
+# In Mac, the elapsed time
+# 19.713
 
 # Function to unreg the registration of parallel
 unregister <- function() {
   env <- foreach:::.foreachGlobals
   rm(list=ls(name=env), pos=env)
 }
+# optional command to unreg parallel backend with foreach package,
+# because we want to run unparallel command. 
 unregister()
-
-# Function to stop cluster
-stopCluster(cl)
-
 
 # 2 Using non parallel programming to run glm training of iris dataset 
 ptime_non <- system.time({
@@ -44,8 +71,14 @@ ptime_non <- system.time({
 })[3]
 ptime_non
 
-# elapsed 
+# elapsed time in Windows
 # 92.74 
+
+# elapsed time in MAC, if getparworker still 3
+# 36.661 
+
+# elapsed time in MAC, if getparworker is 1
+# 37.647 
 
 # Conclusion: parallel programming will make the elapsed time faster than non parallel programming. 
 # For bootstraping 
--- parallel_code_caret_v20161121_final.R
+++ parallel_code_caret_v20161121_final.R
@@ -1,24 +1,38 @@
-#library(parallel)
+# First, we need to make sure that all required packages are installed 
+# install packages: parallel, doParallel, ranger
+install.packages("parallel")
+install.packages("doParallel")
+install.packages("ranger")
+install.packages("mlbench") #optional, because data Sonar will be provided
+install.packages("caret")
+install.packages("e1071")
+
+library(parallel)
 library(caret)
+library(ranger)
 library(mlbench)
 library(doParallel)
 
-#Data import
+# Data import
 data(Sonar)
+
+# In AWS Testing, please coordinate with Amir providing the data in csv format 
+# Sonar <- read.csv("Sonar.csv")
 
 # Calculate the number of cores
 no_cores <- detectCores() - 1
 
 # Initiate cluster
-cl <- makeCluster(no_cores,type = "PSOCK")
+# cl <- makeCluster(no_cores,type = "PSOCK") # this is for WINDOWS
+cl <- makeCluster(no_cores,type="FORK") # this is for UNIX
 registerDoParallel(cl)
 
 # Do training to tune mtry parameter with PARALLEL programming
 system.time(result1 <- train(
-  tuneGrid=data.frame(mtry=c(2:35)),
+  tuneGrid=data.frame(mtry=c(2:50)),
   Class~.,
   data = Sonar, method = "ranger",
-  trControl = trainControl(Sonar, method = "cv", number = 10, verboseIter = TRUE)
+  trControl = trainControl(Sonar, method = "cv", number = 10)
 ))
 
 # Outcome sample
@@ -41,12 +55,12 @@ stopCluster(cl)
 # Check how many cluster how many workers foreach is used
 getDoParWorkers()
 
-# Do training without parallel programming
+# Do training without parallel programming, 
 system.time(result2<- train(
-  tuneGrid=data.frame(mtry=c(2:35)),
+  tuneGrid=data.frame(mtry=c(2:50)),
   Class~.,
   data = Sonar, method = "ranger",
-  trControl = trainControl(Sonar, method = "cv", number = 10, verboseIter = TRUE, allowParallel = FALSE)
+  trControl = trainControl(Sonar, method = "cv", number = 10, allowParallel = FALSE)
 ))
 
 # Outcome sample
