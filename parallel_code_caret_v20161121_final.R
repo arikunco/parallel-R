007c58e84b4f85b753473f38aa4996d932299cdd
Bernardus Ari Kuncoro <ari.kuncoro1987@gmail.com>
2016-11-24 10:47:35 +0700

    Add files via upload

---
+++ parallel_code_caret_v20161121_final.R
@@ -0,0 +1,56 @@
+#library(parallel)
+library(caret)
+library(mlbench)
+library(doParallel)
+
+#Data import
+data(Sonar)
+
+# Calculate the number of cores
+no_cores <- detectCores() - 1
+
+# Initiate cluster
+cl <- makeCluster(no_cores,type = "PSOCK")
+registerDoParallel(cl)
+
+# Do training to tune mtry parameter with PARALLEL programming
+system.time(result1 <- train(
+  tuneGrid=data.frame(mtry=c(2:35)),
+  Class~.,
+  data = Sonar, method = "ranger",
+  trControl = trainControl(Sonar, method = "cv", number = 10, verboseIter = TRUE)
+))
+
+# Outcome sample
+# Selecting tuning parameters
+# Fitting mtry = 3 on full training set
+# user  system elapsed 
+# 2.16    0.06   12.75 
+
+# Function to unreg the registration of parallel
+unregister <- function() {
+  env <- foreach:::.foreachGlobals
+  rm(list=ls(name=env), pos=env)
+}
+
+unregister()
+
+# Function to stop cluster
+stopCluster(cl)
+
+# Check how many cluster how many workers foreach is used
+getDoParWorkers()
+
+# Do training without parallel programming
+system.time(result2<- train(
+  tuneGrid=data.frame(mtry=c(2:35)),
+  Class~.,
+  data = Sonar, method = "ranger",
+  trControl = trainControl(Sonar, method = "cv", number = 10, verboseIter = TRUE, allowParallel = FALSE)
+))
+
+# Outcome sample
+# Selecting tuning parameters
+# Fitting mtry = 3 on full training set
+# user  system elapsed 
+# 35.37    0.71   36.41
